c983945f4b589089585cab05677bdcf7719101a3
Logan Warren <Loganwarrenlaw@gmail.com>
2023-04-03 16:41:33 -0400

    updated code

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
 from flask import Flask, request
- 
+
 app = Flask(__name__)
- 
+
 hackathons = {
-    "GHW: API Week":{
+    "GHW: API Week": {
         "start_date": "2023-04-03 12:00:00",
         "end_date": "2023-04-10 12:00:00",
         "location": "Everywhere, Online",
         "type": "Digital Only"
     },
-    "Bitcamp":{
+    "Bitcamp": {
         "start_date": "2023-04-07 12:00:00",
-        "end_date": "2023-04-9 12:00:00",
+        "end_date": "2023-04-09 12:00:00",
         "location": "College Park, Maryland",
         "type": "In-Person Only"
     }
 }
 
-
-
 @app.route("/")
 def hello_ghw():
-    return 'Thanks 404 for all the Help'
- 
-@app.route('/getHackathons', methods=['GET'])
-def getHackathons():
-    return hackathons_list
+	return "<p>Thanks, 404, for all the help!!!</p>"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/hackathons', methods=['GET', 'POST'])
+def getHackathons():
+	if request.method == 'POST':
+		hackathons["New Hackathon"] = request.json
+		return hackathons
+	else:	
+		return hackathons
+
+if __name__=="__main__":
+	app.run(debug = True)
